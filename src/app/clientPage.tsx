6b5e909011c99c7b0bd9613aeeefafdc1016d273
Carlton Downie <carl@cdownie.co.uk>
2023-03-30 23:27:54 +0100

    video experiment

--- src/app/clientPage.tsx
+++ src/app/clientPage.tsx
@@ -1,15 +1,14 @@
 'use client'
 
 import DecisionTree from '@/components/DecisionTree'
-import DecisionTreeButton from '@/components/DecisionTree/Button'
 import DecisionTreeHistory, {
   DecisionTreeHistoryItem,
 } from '@/components/DecisionTree/History'
-import ExpandingVideo from '@/components/ExpandingVideo'
 import IntroAnimation from '@/components/IntroAnimation'
 import ContentContainer from '@/components/Layout/ContentContainer'
 import ProgressBar from '@/components/ProgressBar'
 import QuoteFader from '@/components/QuoteFader'
+import Heading from '@/components/Typography/Heading'
 import Quotation from '@/components/Typography/Quotation'
 import { getDecisionTreeDepth } from '@/lib/getDecisionTree'
 import PortableTextRenderer from '@/lib/PortableTextRenderer'
@@ -66,86 +65,110 @@ export default function ClientHome({
     setCurrentTree(current)
   }
 
+  const dTreeOpacity = history.length > 0 ? 'bg-opacity-100' : 'bg-opacity-80'
+
   return (
     <>
       <IntroAnimation />
-      <ProgressBar progress={progress} />
+      <ProgressBar progress={progress} className="bg-brand-navy" />
+      <div className="relative">
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full h-full absolute top-0 left-0 object-cover"
+        >
+          <source src="/video/hero-video.mp4" type="video/mp4" />
+        </video>
 
-      <div className="flex flex-col md:flex-row min-h-[80vh] overflow-x-hidden relative bg-brand-navy">
-        <div className="bg-brand-navy flex-1 pb-20 flex justify-center">
-          <div className="max-w-lg w-full min-h-[60vh] md:min-h-[80vh]">
-            <DecisionTreeHistory
-              history={history}
-              onItemPressed={(index) =>
-                replayDecisions(history.slice(0, index).map((x) => x.option))
-              }
-            />
-            <DecisionTree
-              treeData={currentTree}
-              currentStepNumber={history.length + 1}
-              onOptionSelected={(index) => {
-                const newState = selectOption(currentTree, index)
-                if (newState.newState) {
-                  setCurrentTree(newState.newState)
+        <div className="flex flex-col md:flex-row min-h-[80vh] overflow-x-hidden relative bg-brand-navy bg-opacity-0">
+          <div
+            className={`bg-brand-navy flex-1 pb-20 flex justify-center ${dTreeOpacity}`}
+          >
+            <div className="max-w-lg w-full min-h-[60vh] md:min-h-[80vh]">
+              <DecisionTreeHistory
+                history={history}
+                onItemPressed={(index) =>
+                  replayDecisions(history.slice(0, index).map((x) => x.option))
                 }
-                setHistory([
-                  ...history,
-                  {
-                    option: index,
-                    label: currentTree.historyTitle ?? currentTree.title,
-                  },
-                ])
-              }}
-            />
+              />
+              <DecisionTree
+                treeData={currentTree}
+                currentStepNumber={history.length + 1}
+                onOptionSelected={(index) => {
+                  const newState = selectOption(currentTree, index)
+                  if (newState.newState) {
+                    setCurrentTree(newState.newState)
+                  }
+                  setHistory([
+                    ...history,
+                    {
+                      option: index,
+                      label: currentTree.historyTitle ?? currentTree.title,
+                    },
+                  ])
+                }}
+              />
+            </div>
           </div>
-        </div>
-        <AnimatePresence initial={false} mode="popLayout">
-          {history.length === 0 || isMobile ? (
-            <motion.div
-              key={1}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                ease: 'easeOut',
-                duration: 1,
-              }}
-              className="bg-white flex-1 grid grid-rows-[1fr_auto] items-center"
-            >
-              <div className="p-4">
-                <div className="my-5">
-                  <QuoteFader className="text-brand-navy" quotes={quotes} />
-                </div>
-                <a
-                  className="text-brand-navy"
-                  href="https://www.doctify.com/uk/practice/livaware"
-                  target="_blank"
-                  rel="noopener"
+          <AnimatePresence initial={false} mode="popLayout">
+            {history.length === 0 || isMobile ? (
+              <motion.div
+                key={1}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  ease: 'easeOut',
+                  duration: 1,
+                }}
+                className={`bg-brand-navy ${dTreeOpacity} flex-1`}
+              >
+                <Heading
+                  variant="h1"
+                  className="text-white md:mt-16 text-center"
                 >
-                  See more on <span className="text-brand-green">Doctify</span>
-                </a>
-              </div>
-              <video autoPlay muted loop className="w-full aspect-video">
-                <source src="/video/hero-video.mp4" type="video/mp4" />
-              </video>
-            </motion.div>
-          ) : (
-            <motion.div
-              key={2}
-              style={{
-                originX: 0,
-              }}
-              initial={{ scaleX: 0, opacity: 1 }}
-              animate={{ scaleX: 1, opacity: 1 }}
-              exit={{ scaleX: 0, opacity: 1 }}
-              transition={{
-                ease: 'easeOut',
-                duration: 1,
-              }}
-              className="bg-brand-navy flex-1"
-            ></motion.div>
-          )}
-        </AnimatePresence>
+                  Who we are
+                </Heading>
+                <div className="p-4 text-white">
+                  <Quotation
+                    text={`Priding ourselves on anticipating and filling the gaps in
+                  people's healthcare; we combine clinical excellence, precision
+                  organisation and compassion into an unrivalled service.`}
+                    cite="Hemmen Jutla, Founding Clinician"
+                  />
+                </div>
+                <div className="p-4 mt-8">
+                  <QuoteFader className="text-white" quotes={quotes} />
+                  <a
+                    className="text-white"
+                    href="https://www.doctify.com/uk/practice/livaware"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    See more on{' '}
+                    <span className="text-brand-green">Doctify</span>
+                  </a>
+                </div>
+              </motion.div>
+            ) : (
+              <motion.div
+                key={2}
+                style={{
+                  originX: 0,
+                }}
+                initial={{ scaleX: 0, opacity: 1 }}
+                animate={{ scaleX: 1, opacity: 1 }}
+                exit={{ scaleX: 1, opacity: 0 }}
+                transition={{
+                  ease: 'easeOut',
+                  duration: 1,
+                }}
+                className="bg-brand-navy flex-1"
+              ></motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
       <ContentContainer>
         <PortableTextRenderer content={content} />
--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,12 @@
-export default function ProgressBar({ progress }: { progress: number }) {
+export default function ProgressBar({
+  progress,
+  className,
+}: {
+  progress: number
+  className?: string
+}) {
   return (
-    <div className="w-full h-1">
+    <div className={`w-full h-1 ${className}`}>
       <div
         className="bg-brand-green h-1 [transition:width_0.3s]"
         style={{ width: `${progress * 100}%` }}
