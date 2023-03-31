b8f4a509be82c0b47ded7b2bd23f615d3e4c0e36
Carlton Downie <carl@cdownie.co.uk>
2023-03-31 20:56:41 +0100

    add some unit tests

---
+++ src/components/__tests__/ChatBot.tests.tsx
@@ -0,0 +1,12 @@
+// jest unit test for ChatBot react component
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import ChatBot from '../ChatBot'
+
+describe('ChatBot', () => {
+  it('renders ChatBot component', () => {
+    const snapshot = render(<ChatBot />)
+    expect(screen.getByText('Ask Livaware')).toBeInTheDocument()
+    expect(snapshot).toMatchSnapshot()
+  })
+})
