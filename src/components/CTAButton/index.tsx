9b3d4ab1bc1eea8413cf191406e21fbb3041c63a
Carlton Downie <carl@cdownie.co.uk>
2023-04-11 15:00:32 +0100

    remove animation

--- src/components/CTAButton/index.tsx
+++ src/components/CTAButton/index.tsx
@@ -1,9 +1,6 @@
-'use client'
-
 import Link from 'next/link'
 import { twMerge } from 'tailwind-merge'
 import Chevron from '../Icons/Chevron'
-import { motion, useInView } from 'framer-motion'
 import { useRef } from 'react'
 
 export default function CTAButton({
@@ -18,8 +15,6 @@ export default function CTAButton({
   className?: string
 }) {
   const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
-
   const css = twMerge(
     'hover:bg-brand-green text-left w-full p-4 grid grid-cols-[1fr_auto] border-b border-b-black',
     className
@@ -32,16 +27,7 @@ export default function CTAButton({
   )
   if (href) {
     return (
-      <Link
-        href={href}
-        className={css}
-        ref={ref}
-        style={{
-          transform: isInView ? 'none' : 'translateX(-200px)',
-          opacity: isInView ? 1 : 0,
-          transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s',
-        }}
-      >
+      <Link href={href} className={css} ref={ref}>
         {content}
       </Link>
     )
